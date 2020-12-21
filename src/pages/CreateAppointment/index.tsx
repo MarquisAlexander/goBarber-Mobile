a370757229d116b2a12779107d786fe94b482d48
marquisalexander <marquissantos123@gmail.com>
2020-12-21 20:20:00 -0300

    Agendamentos

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useAuth } from '../../hooks/auth';
+
 import { View } from 'react-native';
 
+import { 
+    Container,
+    Header,
+    BackButton,
+    HeaderTitle,
+    UserAvatar,
+    } from './styles';
+
+interface RouteParams {
+    providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+    const { user } = useAuth();
+    const route = useRoute();
+    const { goBack } = useNavigation();
+    const { providerId } = route.params as RouteParams;
+
+    const navigateBack = useCallback(() => {
+        goBack();
+    }, [goBack]);
 
     return (
-        <View />
+        <Container>
+            <Header>
+                <BackButton onPress={navigateBack}>
+                    <Icon name="chevron-left"size={24} color="#999591" />
+                </BackButton>
+
+                <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+                <UserAvatar source={{ uri: user.avatar_url}} />
+            </Header>
+        </Container>
+            
     );
 };
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,11 +2,27 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, Button } from 'react-native';
 import api from '../../services/api';
+import Icon from 'react-native-vector-icons/Feather';
 
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar, ProvidersList } from './styles';
+import { 
+    Container, 
+    Header, 
+    HeaderTitle, 
+    UserName, 
+    ProfileButton, 
+    UserAvatar, 
+    ProvidersList,
+    ProviderContainer,
+    ProviderAvatar,
+    ProviderInfo,
+    ProviderName,
+    ProviderMeta,
+    ProviderMetaText,
+    ProvidersListTitle,
+} from './styles';
 
 export interface Provider {
     id: string;
@@ -30,6 +46,10 @@ const Dashboard: React.FC = () => {
         signOut();
     },[signOut]);
 
+    const navigateToCreateAppointment = useCallback((providerId: string) => {
+        navigate('CreateAppointment', { providerId });
+    }, [navigate])
+
     return (
         <Container>
             <Header>
@@ -48,8 +68,26 @@ const Dashboard: React.FC = () => {
             <ProvidersList 
             data={providers}
             keyExtractor={(provider) => provider.id}
-            renderItem={({ item }) (
+            ListHeaderComponent={
+                <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+            }
+            renderItem={({ item: provider }) => (
+                <ProviderContainer onPress={() => navigateToCreateAppointment{provider.id}}>
+                    <ProviderAvatar source={{ uri: provider.avatar_url }} />
 
+                    <ProviderInfo>
+                        <ProviderName>{provider.name}</ProviderName>
+                        <ProviderMeta>
+                            <Icon name="calendar" size={14} color="#ff9000" />
+                            <ProviderMetaText>Segunda a sexta</ProviderMetaText>
+                        </ProviderMeta>
+
+                        <ProviderMeta>
+                            <Icon name="calendar" size={14} color="#ff9000" />
+                            <ProviderMetaText>Segunda a sexta</ProviderMetaText>
+                        </ProviderMeta>
+                    </ProviderInfo>
+                </ProviderContainer>
             )}
             />
         </Container>
